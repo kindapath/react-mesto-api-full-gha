dacdb01acf757cd3c2cb560843557dbe32f21998
Zhenya Nikolaev <kindaboii@yandex.ru>
2023-05-17 02:44:16 +0300

    bugfix: modify validateEditProfile required fields

--- backend/middlewares/validations.js
+++ backend/middlewares/validations.js
@@ -27,8 +27,8 @@ const validateUserId = celebrate({
 
 const validateEditProfile = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
   }),
 });
 
